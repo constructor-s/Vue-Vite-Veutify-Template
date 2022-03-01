0c596b818e7bb1c18ad7de0a05eb2ce59ef1c4e8
B <10003477+constructor-s@users.noreply.github.com>
2022-03-01 14:35:15 -0500

    Named routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="home">
         <h1>Home</h1>
+        <div class="destinations">
+            <router-link :to="{name: 'destination.show', params: {id: 'Earth'}}"><h2>Earth</h2></router-link>
+            <router-link :to="{name: 'destination.show', params: {id: 'Moon'}}"><h2>Moon</h2></router-link>
+            <router-link :to="{name: 'destination.show', params: {id: 'Mars'}}"><h2>Mars</h2></router-link>
+        </div>
     </div>
 </template>

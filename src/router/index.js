715f489a23cc43589c74fba2c65df526428c281e
B <10003477+constructor-s@users.noreply.github.com>
2022-03-01 14:29:55 -0500

    Lazy, dynamic routes example

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import About from '../views/About.vue'
 const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/about', name: 'About', component: About },
+    { path: '/destination/:id', component: () => import('../views/DestinationShow.vue') }
 ];
 
 const router = createRouter({
---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h2>Hello Destionation: {{$route.params.id}}</h2>
+    </div>
+</template>
